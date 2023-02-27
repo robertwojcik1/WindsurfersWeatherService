300176621b1eea46999d68db23725c730f3c1310
Robert Wójcik <robert.wojcik.programista@gmail.com>
2023-02-27 22:16:33 +0100

    empty project

---
+++ src/main/java/wojcik/robert/windsurfersweatherservice/WindsurfersWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package wojcik.robert.windsurfersweatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfersWeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurfersWeatherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wojcik/robert/windsurfersweatherservice/WindsurfersWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wojcik.robert.windsurfersweatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfersWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
